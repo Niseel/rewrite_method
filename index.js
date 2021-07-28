a82ede4762d097651f4f7a04b2e6932700ee7932
Niseel <nguyencongthanh200199@gmail.com>
2021-07-28 22:18:05 +0700

    reduce, map, find, filter, every, some - fix empty except reduce

--- index.js
+++ index.js
@@ -5,24 +5,32 @@
 // Mảng thử
 // const arr = [1, 3, 20, 5];
 
-// Overwrite
+// // Overwrite
 // Array.prototype.some2 = function (callback) {
 //   if (typeof callback !== "function") {
 //     throw new TypeError(`${callback} is not function`);
 //   }
 //   var result = false;
-
-//   var arrLength = this.length;
-//   for (var i = 0; i < arrLength; i++) {
-//     result = callback(this[i], i, this);
-//     if (result === true) {
-//       break;
+//   // var arrLength = this.length;
+//   // for (var i = 0; i < arrLength; i++) {
+//   //   result = callback(this[i], i, this);
+//   //   if (result === true) {
+//   //     break;
+//   //   }
+//   // }
+//   for (var i in this) {
+//     if (this.hasOwnProperty(i)) {
+//       result = callback(this[i], Number(i), this);
+//       if (result === true) {
+//         break;
+//       }
 //     }
 //   }
+
 //   return result;
 // };
 
-// Gọi hàm thử
+// // Gọi hàm thử
 // var result = arr.some2(function (item, i, arr) {
 //   console.log(item, i, arr);
 //   return item % 2 === 0;
@@ -41,7 +49,7 @@
  * Viết lại phương thức every
  */
 
-// // Mảng thử
+// Mảng thử
 // const arr = [1, 30, 39, 29, 10, 13];
 
 // // Overwrite
@@ -50,20 +58,28 @@
 //     throw new TypeError(`${callback} is not function`);
 //   }
 //   var result = true;
-
-//   var arrLength = this.length;
-//   for (var i = 0; i < arrLength; i++) {
-//     result = callback(this[i], i, this);
-//     if (result === false) {
-//       break;
+//   // var arrLength = this.length;
+//   // for (var i = 0; i < arrLength; i++) {
+//   //   result = callback(this[i], i, this);
+//   //   if (result === false) {
+//   //     break;
+//   //   }
+//   // }
+//   for (var i in this) {
+//     if (this.hasOwnProperty(i)) {
+//       result = callback(this[i], Number(i), this);
+//       if (result === false) {
+//         break;
+//       }
 //     }
 //   }
+
 //   return result;
 // };
 
 // // Gọi hàm thử
 // var result = arr.every2(function (item, i, arr) {
-//   // console.log(item, i, arr);
+//   console.log(item, i, arr);
 //   return item < 30;
 // });
 // console.log(result);
@@ -91,6 +107,7 @@
 //   "present",
 // ];
 // const words = [2, 5, 1, 6, 8, 12, 623, 11];
+// words.length = 100;
 // const words = [
 //   1,
 //   {},
@@ -108,10 +125,10 @@
 //     throw new TypeError(`${callback} is not function`);
 //   }
 //   var result = [];
-
-//   var arrLength = this.length;
-//   for (var i = 0; i < arrLength; i++) {
-//     pushItem = callback(this[i], i, this) ? this[i] : false;
+//   for (var i in this) {
+//     if (this.hasOwnProperty(i)) {
+//       pushItem = callback(this[i], i, this) ? this[i] : false;
+//     }
 //     if (!!pushItem) {
 //       result.push(pushItem);
 //     }
@@ -121,14 +138,14 @@
 // };
 
 // Gọi hàm thử
-
 // const result = words.filter2(function (word, i, arr) {
-//   console.log(word, i, arr);
-//   //return word.length > 16;
-//   //return word % 2 !== 0;
-//   //return typeof word === "object";
+//console.log(word, i, arr);
+// return word == 10;
+//return word.length > 16;
+//return word % 2 !== 0;
+//return typeof word === "object";
 // });
-//console.log(result);
+// console.log(result);
 
 // Gọi hảm nguyên bản
 // const result = words.filter2(function (word, i, arr) {
@@ -145,28 +162,46 @@
 
 // Mảng thử
 // const arr = [5, 12, 8, 130, 44];
+// arr.length = 100;
 
-// Overwrite
+// var arr = new Array(30);
+// arr.push(200);
+
+// // Overwrite
 // Array.prototype.find2 = function (callback) {
 //   if (typeof callback !== "function") {
 //     throw new TypeError(`${callback} is not function`);
 //   }
 
-//   var result;
+//   var result = false;
 //   var arrLength = this.length;
 //   for (var i = 0; i < arrLength; i++) {
-//     result = callback(this[i], i, this);
+//     if (callback(this[i], i, this)) {
+//       result = this[i];
+//     }
 //   }
+
+//   // Theo mô tả của Mozilla thì find sẽ chạy hết phần tử, kể cả phần tử
+//   // empty chưa gán để lọc hết nên k cần dùng for-in
+
+//   // for (var i in this) {
+//   //   if (this.hasOwnProperty(i)) {
+//   //     result = callback(this[i], Number(i), this);
+//   //     if (result) break;
+//   //   }
+//   // }
 //   return result === false ? undefined : result;
 // };
 
-// Gọi hàm thử
-// const found = arr.find2(function (item, i, originArray) {
-//   console.log(item, i, originArray);
+// // Gọi hàm thử
+// const found = arr.find(function (item, i, originArray) {
+//   console.log(item, i);
 //   // return item === 0;
-//   // return item > 10;
+//   return item > 1000;
 // });
 // console.log(found);
+
+// Chưa làm được truyền đối số thứ 2
 
 // Gọi hàm nguyên bản
 // const found = arr.find(function (item, i, originArray) {
@@ -182,6 +217,11 @@
 
 // Mảng thử
 // var arr = [2, 4, 5, 10];
+// arr.length = 100;
+
+// for (var i in arr) {
+//   console.log(arr[i]);
+// }
 
 // Overwrite
 // Array.prototype.forEach2 = function (callback) {
@@ -189,9 +229,10 @@
 //     throw new TypeError(`${callback} is not function`);
 //   }
 
-//   var arrLength = this.length;
-//   for (var i = 0; i < arrLength; i++) {
-//     callback(this[i], i, this);
+//   for (var i in this) {
+//     if (this.hasOwnProperty(i)) {
+//       callback(this[i], Number(i), this);
+//     }
 //   }
 // };
 
@@ -242,10 +283,11 @@
  * Viết lại phương thức reduce
  */
 
-// Mảng thử
+// // Mảng thử
 // var arr = [2, 4, 5, 10];
+// //arr.length = 10;
 
-// Overwrite
+// // Overwrite
 // Array.prototype.reduce2 = function (callback, initialValue) {
 //   if (typeof callback !== "function") {
 //     throw new TypeError(`${callback} is not function`);
@@ -260,16 +302,27 @@
 //     initialValue = callback(initialValue, this[i], i, this);
 //   }
 //   return initialValue;
+//   // Chưa sửa đc empty elements
+//   // if (initialValue === undefined) {
+//   //   initialValue = this[0];
+//   // }
+//   // for (var i in this) {
+//   //   if (this.hasOwnProperty(i)) {
+//   //     initialValue = callback(initialValue, this[i], Number(i), this);
+//   //   }
+//   // }
 // };
 
-// Gọi hàm thử
-// var result = arr.reduce2(function (total, current) {
+// // Gọi hàm thử
+// var result = arr.reduce2(function (total, current, i, originArray) {
+//   console.log(total, current);
 //   return total + current;
-// });
+// }, 0);
 // console.log(result);
 
 // Gọi hàm nguyên bản
-// var result = arr.reduce(function (total, current) {
+// var result = arr.reduce(function (total, current, i, originArray) {
+//   console.log(current, i);
 //   return total + current;
 // }, 0);
 // console.log(result);
