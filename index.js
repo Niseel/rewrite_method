55c27b3f46e410a7238eb8cab4f83408a9c2b8e2
Thanh Cong <47278184+Niseel@users.noreply.github.com>
2022-01-29 17:58:46 +0700

    Update index.js
    change myFilter

--- index.js
+++ index.js
@@ -136,6 +136,26 @@
 
 //   return result;
 // };
+// filter k dung. pushItem not defined
+// example
+
+//
+// The global variable
+// const s = [23, 65, 98, 5];
+
+// Array.prototype.myFilter = function(callback) {
+//   const newArray = [];
+//   for(let i=0;i<this.length;i++){
+//     if(callback(this[i])===true){
+//       newArray.push(this[i]);
+//     }
+//   }
+//   return newArray;
+// };
+
+// const new_s = s.myFilter(function(item) {
+//   return item % 2 === 1;
+// });
 
 // Gọi hàm thử
 // const result = words.filter2(function (word, i, arr) {
